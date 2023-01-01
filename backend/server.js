58c5d47bb464e47ca6c749c02cf6c560f3730f2c
Helondale <113353417+Helondale@users.noreply.github.com>
2023-01-01 14:37:05 +0100

    Added all tasks

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,39 @@
 // Write the Backend tasks here
+let cica = "Server";
+let timeOut = 1000;
+console.log(cica);
+let kacsa = cica.split("").reverse().join("");
+console.log(kacsa);
+
+function numberWords(cica){
+   let words = cica.split(' ')
+   let wordsLength = []
+   words.forEach((cica) => {
+    wordsLength.push(cica + " = " +cica.length)
+
+   });
+   return wordsLength
+};
+console.log(numberWords(cica));
+
+function threeWord(cica){
+    let words = cica.split(' ')
+    let wordsLength = []
+    words.forEach((cica) => {
+     wordsLength.push(cica + " = " +(cica.length*3))
+ 
+    });
+    return wordsLength
+};
+console.log(threeWord(cica));
+
+function fiveWord(cica){
+    let words = cica.split(' ')
+    let wordsLength = []
+    words.forEach((cica) => {
+     wordsLength.push(cica + " = " +(cica.length/5))
+ 
+    });
+    return wordsLength
+};
+console.log(fiveWord(cica));
